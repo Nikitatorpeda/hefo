468005659539827d08c13ad529c0496cf5df4f8d
Nikitatorpeda <166994519+Nikitatorpeda@users.noreply.github.com>
2024-04-28 12:47:36 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -6,8 +6,37 @@ win_height=500
 window = display.set_mode((win_width,win_height))
 window.fill(back)
 
-game = True
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height)) 
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
 
+    
+
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+    def update_l(self):
+        keys=key.get_pressed()
+        if keys[K_w] and self.rect.y>5:
+            self.rect.y-=self.speed 
+        if keys[K_s] and self.rect.y<500:
+            self.rect.y+=self.speed 
+    def update_r(self):
+        keys=key.get_pressed()
+        if keys[K_UP] and self.rect.y>5:
+            self.rect.y-=self.speed 
+        if keys[K_DOWN] and self.rect.y<500:
+            self.rect.y+=self.speed
+game = True
+racket1 = Player('...',30,200,4,50,150)
+racket2 = Player('...',530,200,4,50,150)
+ball = GameSprite('...',200,200,4,50,50)
 finish=False 
 clock = time.Clock()
 FPS =60
@@ -18,3 +47,4 @@ while game:
             game = False 
     display.update()
     clock.tick(FPS)
+    
